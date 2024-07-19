22a25e15dd736fe954d1a8923b72c445709d57e2
Duashah <89727592+Duashah@users.noreply.github.com>
2024-07-19 15:52:49 +0500

    Add files via upload

---
+++ dua.py
@@ -0,0 +1,13 @@
+def my_self():
+    print("i am dua shah, graduated from muet, age 24")
+
+
+def greetings():
+    print("hello Good evening")
+
+
+print(__name__)
+if  __name__ == "__main__":
+    my_self()
+else:
+    print("no file exist")
